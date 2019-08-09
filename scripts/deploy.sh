03a39fbe5840ab896b387a74a296d2ad3f49bf81
Jinwoo Shin <bluesky0255@gmail.com>
2019-08-09 21:19:18 +0900

    Edit syntax correction

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,13 @@ docker push laterality/jwp-blog:latest
 
 container_id_stop=$(docker ps -q --filter name=blog)
 
-if [ ${#container_id_stop}!=0 ] then
+if [ ${#container_id_stop}!=0 ]; then
     docker stop blog
 fi
 
 container_id_remove=$(docker ps -a -q -filter name=blog)
 
-if [ ${#container_id_remove}!=0 ] then
+if [ ${#container_id_remove}!=0 ]; then
     docker rm blog
 fi
 
